638b0e189cf5f9d060167050305c7e33b25d943f
Joanna Edgell <joedgell2013@gmail.com>
2014-12-01 11:22:24 -0500

    added div for ligin/signup

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -277,13 +277,24 @@ $( document ).ready(function(){
 
     routes: {
       '' : 'home',
-      'profile/id:' : 'profileInfo',
+      'start': 'enterSite',
+      'profile' : 'profileInfo',
+
     },
 
     home: function() {
+
+    },
+
+    enterSite: function() {
+
+    },
+
+    profileInfo: function() {
       new App.Views.UserProfileView ();
       new App.Views.MyKidsView();
       new App.Views.MyKidsList({collection: App.all_myKids});
+      $('.enterSite').hide();
     }
 
   });
