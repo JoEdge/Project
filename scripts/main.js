eb472c4b3c9141df1eca0a62d83d68f65f6eb610
Joanna Edgell <joedgell2013@gmail.com>
2014-12-09 21:59:30 -0500

    fixed bug in edit event

--- scripts/main.js
+++ scripts/main.js
@@ -54,6 +54,7 @@ $( document ).ready(function(){
       eventName: '',
       eventDate: '',
       location: '',
+      location2: '',
       user:'',
       kids: []
 
@@ -324,6 +325,7 @@ $( document ).ready(function(){
         success: function(user){
           App.user = user;
           App.updateUser();
+          App.router.navigate('', {trigger: true});
         },
 
         error: function(user, error) {
@@ -334,7 +336,7 @@ $( document ).ready(function(){
 
       //clear my form
       $("#loginForm")[0].reset();
-      App.router.navigate('', { trigger: true });
+      // App.router.navigate('', { trigger: true });
     },
 
   });
@@ -589,6 +591,7 @@ $( document ).ready(function(){
       // Return to home page
       App.router.navigate('', {trigger: true});
 
+
     },
 
     deleteKid: function (e) {
@@ -601,7 +604,6 @@ $( document ).ready(function(){
       App.router.navigate('', {trigger: true});
 
     },
-
 
   });
 
@@ -640,6 +642,7 @@ $( document ).ready(function(){
         eventName: $('#eventName').val(),
         eventDate: $('#eventDate').val(),
         location: $('#location').val(),
+        location2: $('#location2').val(),
         user: App.user,
 
       });//end var myEvent
@@ -741,14 +744,15 @@ $( document ).ready(function(){
       e.preventDefault();
 
       // Update our Model Instance
-      this.options.events.set({
+      this.options.eventOne.set({
         eventName: $("#update_event_name").val(),
         eventDate: $("#update_event_date").val(),
         location: $("#update_event_location").val(),
+        location2: $("#update_event_location2").val(),
       });
 
       // Save Instance
-      this.options.events.save();
+      this.options.eventOne.save();
 
       // Return to home page
       App.router.navigate('event', {trigger: true});
@@ -1047,9 +1051,6 @@ $( document ).ready(function(){
       $('.enterSite').hide();
       new App.Views.MyKidsList({collection: App.all_myKids});
       new App.Views.MessageList({collection: App.all_messages});
-      $('#homeBtn').click(function() {
-        location.reload();
-      });
     },
 
     enterSite: function() {
@@ -1059,19 +1060,19 @@ $( document ).ready(function(){
       if(App.user) return App.router.navigate('start', {trigger: true});
         new App.Views.SignUp();
         new App.Views.Login();
-      $('#logOut').click(function() {
-          location.reload();
-      });
+      // $('#logOut').click(function() {
+      //     location.reload();
+      // });
 
     },
 
-    profileInfo: function() {
-      $('.enterSite').show();
-      $('.main').hide();
-      $('.sidebar').hide();
-      var u = App.all_users.get(id);
-      new App.Views.EditUser({users:u});
-    },
+    // profileInfo: function() {
+    //   $('.enterSite').show();
+    //   $('.main').hide();
+    //   $('.sidebar').hide();
+    //   var u = App.all_users.get(id);
+    //   new App.Views.EditUser({users:u});
+    // },
 
     oneKid: function (id) {
       $('.enterSite').show();
@@ -1079,9 +1080,9 @@ $( document ).ready(function(){
       $('.sidebar').hide();
       var sk = App.all_myKids.get(id);
       new App.Views.SoloKid({onekid:sk});
-      $('#logOut').click(function() {
-        location.reload();
-      });
+      // $('#logOut').click(function() {
+      //   location.reload();
+      // });
     },
 
     myKidsInfo: function() {
@@ -1130,12 +1131,7 @@ $( document ).ready(function(){
       $('.sidebar').hide();
       var e = App.all_events.get(id);
       new App.Views.EditEvent({eventOne: e});
-      $('#editor').click(function() {
-        location.reload();
-      });
-      $('#eventDelete').click(function() {
-        location.reload();
-      });
+
     },
 
     editKidInfo: function(id) {
@@ -1192,20 +1188,11 @@ $( document ).ready(function(){
 
     });//end of fetch all_mykids
 
-
-      // $(".dropdown-button").on('click', function() {
-      //   $(".dropdown-menu").toggleClass("show-menu");
-      //   $(".dropdown-menu > li").on('click',function(){
-      //     $(".dropdown-menu").removeClass("show-menu");
-      //   });
-      //   $(".dropdown-menu.dropdown-select > li").on('click',function() {
-      //     $(".dropdown-button").html($(this).html());
-      //   });
-
     //Home Button force refresh
     $('#homeBtn').click(function() {
       location.reload();
     });
+
 
     // Log Out
     $('#logOut').on('click', function (e) {
@@ -1216,6 +1203,9 @@ $( document ).ready(function(){
       App.router.navigate('start', {trigger: true});
     });//end of logout function
 
+    $('#logOut').click(function() {
+      location.reload();
+    });
 
     // Update User
     App.updateUser = function (){
